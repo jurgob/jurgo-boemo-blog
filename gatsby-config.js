a20eb80760f466c2759715bc8c92442488aefaac
jurgob <jurgob@users.noreply.github.com>
2024-03-04 12:49:37 +0000

    track outbound links

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-google-analytics-track-links`,
           `gatsby-remark-reading-time`,
           {
             resolve: `gatsby-remark-images`,
